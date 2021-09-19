c74c1e02f1b0361a6d1f2efad12e591ad90c2215
LST <wer1264@gmail.com>
2021-09-19 14:25:44 +0900

    재귀 (피보나치 수 5)

---
+++ BAEKJOON/10. 재귀/10870.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+input = +input[0];
+
+function fibonacci(n) {
+  if (n === 0) {
+    return 0;
+  } else if (n === 1) {
+    return 1;
+  } else {
+    return fibonacci(n - 1) + fibonacci(n - 2);
+  }
+}
+
+console.log(fibonacci(input));
